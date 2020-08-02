40cf86d1e5c3ff0456e9dc8abd1b01cd681826bb
GlenRapaj <46382374+GlenRapaj@users.noreply.github.com>
2020-08-02 12:18:02 +0200

    Update huffmanObject.java

--- Huffman/src/hufman/huffmanObject.java
+++ Huffman/src/hufman/huffmanObject.java
@@ -2,58 +2,14 @@ package hufman;
 
 public class huffmanObject {
 
-	private String nodeName ;  // Mban emrin e nyjes
-	
-	private int nodeWeght ;  // mban vleren nr.
-
-	public huffmanObject(String nodeName, int nodeWeght) {
-		super();
-		this.nodeName = nodeName;
-		this.nodeWeght = nodeWeght;
-	}
+		public int vlNr ;
+	public char c ;
+	public HuffmanNode left ;
+	public HuffmanNode right ;
 	
 	
-	public huffmanObject( int nodeWeght) {
+	public HuffmanNode() {
 		super();
-		this.nodeWeght = nodeWeght;
-	}
-	
-	public huffmanObject(String nodeName ) {
-		super();
-		this.nodeName = nodeName;
-		
-	}
-
-
-	public huffmanObject() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-
-	public String getNodeName() {
-		return nodeName;
-	}
-
-
-	public void setNodeName(String nodeName) {
-		this.nodeName = nodeName;
-	}
-
-
-	public int getNodeWeght() {
-		return nodeWeght;
-	}
-
-
-	public void setNodeWeght(int nodeWeght) {
-		this.nodeWeght = nodeWeght;
-	}
-
-
-	@Override
-	public String toString() {
-		return "huffmanObject [nodeName=" + nodeName + ", nodeWeght=" + nodeWeght + "]";
 	}
 	
 	
