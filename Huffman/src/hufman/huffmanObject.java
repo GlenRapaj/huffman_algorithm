bcb2e894123efdc517aa539a40b89277066d12c7
GlenRapaj <46382374+GlenRapaj@users.noreply.github.com>
2021-10-01 13:03:08 +0200

    Clean-code

--- Huffman/src/hufman/huffmanObject.java
+++ Huffman/src/hufman/huffmanObject.java
@@ -2,16 +2,12 @@ package hufman;
 
 public class HuffmanNode{
 
-		public int vlNr ;
+	public int vlNr ;
 	public char c ;
 	public HuffmanNode left ;
 	public HuffmanNode right ;
 	
-	
 	public HuffmanNode() {
 		super();
-	}
-	
-	
-	
+	}	
 }
